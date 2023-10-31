97034d557c6dd4d46caeb75f5791ab6455ee07b3
DroidSaurav <131275580+DroidSaurav@users.noreply.github.com>
2023-10-31 17:54:52 +0530

    Code Reduced

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -81,7 +81,7 @@ const dummyMeals = [
       'Cut the tomatoes and the onion into small pieces.',
       'Boil some water - add salt to it once it boils.',
       'Put the spaghetti into the boiling water - they should be done in about 10 to 12 minutes.',
-      'In the meantime, heaten up some olive oil and add the cut onion.',
+      'In the meantime, heat up some olive oil and add the onion.',
       'After 2 minutes, add the tomato pieces, salt, pepper and your other spices.',
       'The sauce will be done once the spaghetti are.',
       'Feel free to add some cheese on top of the finished dish.'
@@ -143,7 +143,7 @@ const dummyMeals = [
       'Form 2 patties',
       'Fry the patties for c. 4 minutes on each side',
       'Quickly fry the buns for c. 1 minute on each side',
-      'Bruch buns with ketchup',
+      'Brush buns with ketchup',
       'Serve burger with tomato, cucumber and onion'
     ],
     isGlutenFree: false,
@@ -179,7 +179,7 @@ const dummyMeals = [
       'Heat the butter and oil in a large pan (allow the fat to get very hot) and fry the schnitzels until golden brown on both sides.',
       'Make sure to toss the pan regularly so that the schnitzels are surrounded by oil and the crumbing becomes ‘fluffy’.',
       'Remove, and drain on kitchen paper. Fry the parsley in the remaining oil and drain.',
-      'Place the schnitzels on awarmed plate and serve garnishedwith parsley and slices of lemon.'
+      'Place the schnitzels on a warmed plate and serve garnished with parsley and slices of lemon.'
     ],
     isGlutenFree: false,
     isVegan: false,
@@ -213,7 +213,7 @@ const dummyMeals = [
     steps: [
       'Wash and cut salad and herbs',
       'Dice the salmon',
-      'Process mustard, vinegar and olive oil into a dessing',
+      'Process mustard, vinegar and olive oil into a dressing',
       'Prepare the salad',
       'Add salmon cubes and dressing'
     ],
@@ -235,7 +235,7 @@ const dummyMeals = [
         'https://cdn.pixabay.com/photo/2017/05/01/05/18/pastry-2274750_1280.jpg',
     duration: 240,
     ingredients: [
-      '4 Sheets of Gelatine',
+      '4 Sheets of Gelatin',
       '150ml Orange Juice',
       '80g Sugar',
       '300g Yoghurt',
@@ -243,7 +243,7 @@ const dummyMeals = [
       'Orange Peel',
     ],
     steps: [
-      'Dissolve gelatine in pot',
+      'Dissolve gelatin in pot',
       'Add orange juice and sugar',
       'Take pot off the stove',
       'Add 2 tablespoons of yoghurt',
@@ -311,7 +311,7 @@ const dummyMeals = [
     ],
     steps: [
       'Slice and fry the chicken breast',
-      'Process onion, garlic and ginger into paste and sauté everything',
+      'Process onion, garlic and ginger into paste and saute everything',
       'Add spices and stir fry',
       'Add chicken breast + 250ml of water and cook everything for 10 minutes',
       'Add coconut milk',
