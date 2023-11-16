d8fa39216394f3899fcacc60ebd51bd7024bc1b4
DroidSaurav <131275580+DroidSaurav@users.noreply.github.com>
2023-11-16 18:01:44 +0530

    Connected Different providers

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -4,7 +4,6 @@ import 'package:cook_it/screens/filters.dart';
 import 'package:cook_it/screens/meals.dart';
 import 'package:cook_it/widgets/main_drawer.dart';
 import 'package:flutter/material.dart';
-import 'package:cook_it/providers/meals_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cook_it/providers/filters_provider.dart';
 
@@ -46,23 +45,7 @@ class _TabsScreenState extends ConsumerState<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final meals = ref.watch(mealsProvider);
-    final activeFilters = ref.watch(filtersProvider);
-    final availableMeals = meals.where((meal) {
-      if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
-        return false;
-      }
-      if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
-        return false;
-      }
-      if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
-        return false;
-      }
-      if (activeFilters[Filter.vegan]! && !meal.isVegan) {
-        return false;
-      }
-      return true;
-    }).toList();
+    final availableMeals = ref.watch(filteredMealsProvider);
 
     Widget activePage = CategoriesScreen(
       availableMeals: availableMeals,
